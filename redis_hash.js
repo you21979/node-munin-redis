8e4b1f089da5732db71d101ba115648fb6882cd8
you21979 <you2197901@gmail.com>
2014-03-03 19:13:51 +0900

    mod

--- redis_hash.js
+++ redis_hash.js
@@ -5,6 +5,22 @@ var munin = require('munin-plugin');
 var filename = __filename.replace(__dirname+'/', '').replace('.js','');
 var jsonfile = '/opt/node-munin-redis/munin-redis.json';
 
+var select = function(opt){
+    var Model = munin.Model.Default;
+    switch(opt){
+    case 'counter':
+        Model = munin.Model.Counter;
+        break;
+    case 'temperature':
+        Model = munin.Model.Temperature;
+        break;
+    case 'rate':
+        Model = munin.Model.Rate;
+        break;
+    }
+    return Model;
+}
+
 var render = function(g){
     g.sortLabel();
     munin.create(g);
@@ -12,12 +28,13 @@ var render = function(g){
 var main = function(config){
     var g = new munin.Graph(config['title'],config['label'],config['category']);
     var rcl = redis.createClient(config['port'],config['host']);
+    var Model = select(config['graph']);
     rcl.hgetall(config['key'], function(err, val){
         if(err){
             return;
         }
         for(var key in val){
-            g.add(new munin.Model.Default(key).setValue(parseFloat(val[key])));
+            g.add(new Model(key).setValue(parseFloat(val[key])));
         }
     });
     rcl.quit(function(err){
